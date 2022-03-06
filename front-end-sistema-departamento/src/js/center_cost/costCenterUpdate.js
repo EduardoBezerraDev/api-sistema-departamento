6537690c9642b14c98ecac2c2712e1bfba16c76d
EduardoBezerraDev <92584609+EduardoBezerraDev@users.noreply.github.com>
2022-03-06 16:37:54 -0300

    .

--- front-end-sistema-departamento/src/js/center_cost/costCenterUpdate.js
+++ front-end-sistema-departamento/src/js/center_cost/costCenterUpdate.js
@@ -6,9 +6,9 @@ function costCenterUpdate(form) {
         window.location.href = "../../index.html"
         return false
     }
-
     const id = document.querySelector("#id").value
     const json = formToJson(form)
+
     axios.put(`${domain}/cost-centers/${id}`, json, header)
         .then(function(response) {
             alert("Centro de custo atualizado com sucesso")
@@ -21,10 +21,9 @@ function costCenterUpdate(form) {
 }
 
 function costCenterUpdateModal(id) {
-    axios.get(`${domain}/cost-centers/${id}`)
+    axios.get(`${domain}/cost-centers/${id}`, header)
         .then(function(response) {
             response.data.forEach(info => {
-                alert(info.id)
                 document.querySelector("#id").value = info.id
                 document.querySelector("#cost_center").value = info.cost_center
             });
